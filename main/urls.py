4c0c873eeb0ba83146ca604c3dd7ba4f2d9a671b
Kirill Kirsanov <montego@yandex.ru>
2022-08-08 18:43:24 +0300

    Added logout functionality

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('login', LoginView.as_view(template_name='form.html', extra_context={'title': 'Войти'}), name='login'),
+    path('logout', LogoutView.as_view(), name='logout')
 ]
