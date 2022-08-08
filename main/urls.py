ecd4832a25301ab5cca6ef8cf3a575c31511ed3e
Kirill Kirsanov <montego@yandex.ru>
2022-08-08 18:36:34 +0300

    Added login function

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
+    path('signup', views.signup, name='signup'),
+    path('login', LoginView.as_view(template_name='form.html', extra_context={'title': 'Войти'}), name='login'),
 ]
