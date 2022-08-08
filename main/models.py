fb4214afa9a44b65a0dceef80c9d8757e871847d
Kirill Kirsanov <montego@yandex.ru>
2022-08-08 20:03:09 +0300

    Added model profile

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='media/avatars')
+    city = models.CharField(max_length=100, null=True)
+    birthday = models.DateField(null=True)
+    bio = models.CharField(max_length=500, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
